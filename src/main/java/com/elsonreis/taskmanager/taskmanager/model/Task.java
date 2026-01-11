ce184be5481cdcb27be4dba040c5ca89af7c58df
elsonreis77 <elsonreis084@gmail.com>
2026-01-11 10:25:31 -0300

    feat: add task service layer

--- src/main/java/com/elsonreis/taskmanager/taskmanager/model/Task.java
+++ src/main/java/com/elsonreis/taskmanager/taskmanager/model/Task.java
@@ -27,5 +27,53 @@ public class Task {
         this.completed = false;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
+
     // getters and setters
 }
--- src/main/java/com/elsonreis/taskmanager/taskmanager/service/TaskService.java
+++ src/main/java/com/elsonreis/taskmanager/taskmanager/service/TaskService.java
@@ -1,4 +1,39 @@
 package com.elsonreis.taskmanager.taskmanager.service;
 
+import com.elsonreis.taskmanager.taskmanager.model.Task;
+import com.elsonreis.taskmanager.taskmanager.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
 public class TaskService {
+        private final TaskRepository repository;
+
+        public TaskService(TaskRepository repository) {
+            this.repository = repository;
+        }
+
+        public Task create(Task task) {
+            return repository.save(task);
+        }
+
+        public List<Task> findAll() {
+            return repository.findAll();
+        }
+
+        public Task complete(Long id) {
+            Task task = repository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Task not found"));
+
+            task.setCompleted(true);
+            task.setCompletedAt(LocalDateTime.now());
+
+            return repository.save(task);
+        }
+
+        public void delete(Long id) {
+            repository.deleteById(id);
+        }
 }
