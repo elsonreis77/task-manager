f1c4cbb2506dd7c2f52a8bceda4b1e4aa046b502
elsonreis77 <elsonreis084@gmail.com>
2026-01-12 18:04:35 -0300

    feat: add Task REST controller

--- src/main/java/com/elsonreis/taskmanager/taskmanager/controller/TaskController.java
+++ src/main/java/com/elsonreis/taskmanager/taskmanager/controller/TaskController.java
@@ -1,4 +1,39 @@
 package com.elsonreis.taskmanager.taskmanager.controller;
 
+
+import com.elsonreis.taskmanager.taskmanager.model.Task;
+import com.elsonreis.taskmanager.taskmanager.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService service;
+
+    public TaskController(TaskService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Task create(@RequestBody Task task) {
+        return service.create(task);
+    }
+
+    @GetMapping
+    public List<Task> findAll() {
+        return service.findAll();
+    }
+
+    @PutMapping("/{id}/complete")
+    public Task complete(@PathVariable Long id) {
+        return service.complete(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
 }
